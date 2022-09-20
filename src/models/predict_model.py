417d4ef3dce2db03cd1c296246076caf010cc3d8
borisllona <borisllonalonso@gmail.com>
2022-09-20 14:51:25 +0200

    pre-commit test

---
+++ src/models/predict_model.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+
+import torch
+from torchvision import datasets, transforms
+
+
+import helper
+import fc_model
+
+import torch.nn.functional as F
+
+from torch import nn
+from torch import optim
